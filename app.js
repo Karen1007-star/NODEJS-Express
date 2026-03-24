19ee7fe7c6723e811c01fe8c23f73507341bd080
Selene <karen_08_121@hotmail.com>
2026-03-24 12:47:16 -0500

    postman

--- app.js
+++ app.js
@@ -28,6 +28,53 @@ app.get('/search', (req, res) => {
             <p>Término: ${terms}</p>
             <p>Categoría: ${category}</p>`);
 });
+///////////////POST
+app.post('/form', (req, res) => {
+    const name = req.body.name || "Anonimo";
+    const email = req.body.email || "nada";
+    
+    res.json({
+        message: 'datos recibidos',
+        data:{
+            name,email
+        }
+    })
+    // Validación para asegurar que recibimos datos válidos
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({
+            error: "No se recibieron datos"
+        });
+    }
+    
+    res.status(200).json({
+        message: "Datos JSON recibidos",
+        data
+    });
+});
+///////////////POST
+app.post('/api/data', (req, res) => {
+    const clave1 = req.body.clave1 || "no hay";
+    const clave2 = req.body.clave2 || "no hay";
+    
+    res.json({
+        message: 'JSON recibidos',
+        data:{
+            clave1,clave2
+        }
+    })
+    // Validación para asegurar que recibimos datos válidos
+    if (!data || Object.keys(data).length === 0) {
+        return res.status(400).json({
+            error: "No se recibieron datos"
+        });
+    }
+    
+    res.status(200).json({
+        message: "Datos JSON recibidos",
+        data
+    });
+});
+
 app.listen(PORT, ()=>{
     console.log("Funcionando");
 })
